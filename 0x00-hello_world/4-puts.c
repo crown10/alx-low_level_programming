30fd808326b203ad72123384b9d5897c6f1a5960
crown10 <pamahcrown@gmail.com>
2022-06-30 09:29:23 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: A C program that prints a sting using puts
- * Return: 0 - SUCCESS
-*/
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+   puts("\"Programming is like building a multilingual puzzle");
+   return(0);
+}   
