f3b7165c1bab1c4f665af183186900d8659561a0
crown10 <pamahcrown@gmail.com>
2022-06-30 08:36:20 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry Point
  *
- * Description: A C program that prints a string using puts
- *
+ * Description: A C program that prints a sting using puts
  * Return: 0 - SUCCESS
- */
+*/
+
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
