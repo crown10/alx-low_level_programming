4e8471e8f5ce797d5193bea407e648fd31ff28b6
crown10 <pamahcrown@gmail.com>
2022-06-30 07:00:58 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints a string using puts
+ *
+ * Return: 0 - SUCCESS
+ */
 
-
-
-	
-puts("\"Programming is like building a multilingual puzzle");
-return 0
-}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}	
