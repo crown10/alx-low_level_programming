9e211b5ea8a984082bbb517c3d2eafb23c05ca5e
crown10 <pamahcrown@gmail.com>
2022-07-01 09:40:26 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,10 @@
  *  * Description: Get a random number and print the number
  *  * and if it is positive, negative, or zero
  *  * Return: 0
- */
+ **/
+
 int main(void)
+
 {
 	int n;
 
@@ -17,7 +19,10 @@ int main(void)
 		printf("%i is positive\n", n);
 	else if (n < 0)
 		printf("%i is negative\n", n);
-	else											
-		printf("%i is zero\n", 
+	else
+
+		printf("%i is zero\n", n);
+
 	return (0);
+
 }
