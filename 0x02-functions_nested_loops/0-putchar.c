bc2296d9798b5667ccaebec84e802e65bb8b97a4
crown10 <pamahcrown@gmail.com>
2022-07-05 05:11:26 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <unistd.h>
 #include <main.h>
+
 /**
- * main print putchar
- * Return: always 0
- */
+ * *main - print putchar
+ * *Return: always 0
+ * **/
 int main(void)
 {
-	_putchar("_putchar"\n);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
 
 	return (0);
 }
