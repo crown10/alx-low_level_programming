32214f6b1f43e7c7ca2a28f719e8177527474f0c
crown10 <pamahcrown@gmail.com>
2022-08-11 11:17:20 -0700

    Task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,9 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -15,11 +15,12 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
